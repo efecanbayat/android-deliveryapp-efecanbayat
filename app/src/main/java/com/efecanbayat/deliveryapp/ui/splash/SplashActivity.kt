2e1228f6782c2dcd977a98aa2a55fc3359514583
Efecan Bayat <efecanbayat@gmail.com>
2021-08-16 14:21:17 +0300

    added navigation component and splash screen

---
+++ app/src/main/java/com/efecanbayat/deliveryapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.efecanbayat.deliveryapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.efecanbayat.deliveryapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/com/efecanbayat/deliveryapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,52 @@
+package com.efecanbayat.deliveryapp.ui.splash
+
+import android.animation.Animator
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.efecanbayat.deliveryapp.databinding.FragmentSplashBinding
+
+class SplashFragment: Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        binding.splashAnimation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+                Log.v("Splash","Animation Started")
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Log.v("Splash","Animation Finished")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Log.v("Splash","Animation Canceled")
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                Log.v("Splash","Animation Repeated")
+            }
+
+        })
+    }
+
+
+}
