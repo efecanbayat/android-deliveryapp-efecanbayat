a7b5f26b0e42da76f874596a999079c8942122ab
Efecan Bayat <efecanbayat@gmail.com>
2021-08-17 21:56:31 +0300

    activated bottom navigation bar

--- app/src/main/java/com/efecanbayat/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/efecanbayat/deliveryapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.efecanbayat.deliveryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.efecanbayat.deliveryapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navController = Navigation.findNavController(this, R.id.mainFragmentContainer)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
+    }
+
+
 }
