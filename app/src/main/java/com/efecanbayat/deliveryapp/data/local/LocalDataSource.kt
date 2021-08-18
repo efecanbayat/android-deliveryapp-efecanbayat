9994d5a8b9ad986a49b4c763f83f107d1c7dce39
Efecan Bayat <efecanbayat@gmail.com>
2021-08-18 17:39:27 +0300

    added dagger-hilt to project

---
+++ app/src/main/java/com/efecanbayat/deliveryapp/data/local/LocalDataSource.kt
@@ -0,0 +1,8 @@
+package com.efecanbayat.deliveryapp.data.local
+
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(private val sharedPrefManager: SharedPrefManager) {
+
+
+}
---
+++ app/src/main/java/com/efecanbayat/deliveryapp/di/NetworkModule.kt
@@ -0,0 +1,13 @@
+package com.efecanbayat.deliveryapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+
+@Module
+@InstallIn(
+    ActivityRetainedComponent::class
+)
+class NetworkModule {
+
+}
